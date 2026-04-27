2d2ef2892e6aefff260ef38241ddcb9c1fd0cf74
OldNan <jacobpop123@gmail.com>
2026-04-28 04:37:15 +0800

    added database

--- studyquest/app/__init__.py
+++ studyquest/app/__init__.py
@@ -1,5 +1,14 @@
-from flask import Flask
+import os
+import secrets
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from app import routes
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+from app import routes, models
