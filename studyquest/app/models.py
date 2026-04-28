b6792493965ce57fd4f8481f276b3f2f55d8a88c
jacobpopal1 <jacobpopal1@gmail.com>
2026-04-28 18:06:26 +0800

    added / merged models.py

--- studyquest/app/models.py
+++ studyquest/app/models.py
@@ -1,5 +1,19 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+    
+@dataclass
+class Quest:
+    id: int
+    title: str
+    description: str
+    quest_type: str
+    difficulty: str
+    due_date: Optional[date] = None
+    status: str = "In Progress"
+    date_completed: Optional[date] = None 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
