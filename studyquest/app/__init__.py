47532e6e92ac16b7cf274d388197a6601b14418c
jacobpopal1 <jacobpopal1@gmail.com>
2026-04-28 18:03:25 +0800

    added database setup files

--- studyquest/app/__init__.py
+++ studyquest/app/__init__.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.secret_key = os.environ.get("SECRET_KEY", secrets.token_hex(32))
+
 db = SQLAlchemy(app)
 
+@app.context_processor
+def inject_auth():
+    return {
+        "is_logged_in": "user_id" in session,
+        "username": session.get("username")
+    }
 
 from app import routes, models
