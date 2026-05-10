6ff8e68f7b2f6c219a4504e5cb6174d77b7c4e57
jacobpopal1 <jacobpopal1@gmail.com>
2026-05-10 19:28:16 +0800

    added checklist notes in security to make sure we've fulfilled requirements and fixed error in dashboard.html

--- studyquest/app/security.py
+++ studyquest/app/security.py
@@ -7,3 +7,12 @@ def is_strong_password(password):
         re.search(r'\d', password) and
         re.search(r'[!@#$%^&*(),.?":{}|<>_\-]', password)
     )
+
+#security checklist:
+#csrf protection with wtforms
+#strongpassword check function 
+#rate limiting
+#authentication login with flask login
+#password security with werkzeug (salted hash)
+#route protection via login_required
+#sql injection prevention via SQLAlchemy ORM
