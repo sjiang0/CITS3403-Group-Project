519a1a82b0fdcad6d967301d75022868ba97fabb
jacobpopal1 <jacobpopal1@gmail.com>
2026-05-11 19:49:34 +0800

    config.py refactoring

--- studyquest/app/config.py
+++ studyquest/app/config.py
@@ -1,9 +1,13 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
 
 class TestConfig(Config):
     TESTING = True
